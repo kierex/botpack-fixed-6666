7975b4214a81a189fcf87c6a1a20a87c1b3f5387
kierex <heroxhonattackz20@gmail.com>
2025-05-06 14:48:53 +0800

    Update tid.js

--- modules/commands/tid.js
+++ modules/commands/tid.js
@@ -1,18 +1,17 @@
 module.exports.config = {
- name: "tid",
- version: "1.0.0",
-    hasPermssion: 0,
-    description: "get thread id",
-    credits: "jonell magallanes",
+    name: "tid",
+    version: "1.0.0",
+    hasPermission: 0,  // Fixed the typo here
+    description: "Get thread ID",
+    credits: "Vern",
     usePrefix: true,
     hide: true,
     commandCategory: "System",
     cooldowns: 0,
-  };
+};
+
 module.exports.run = async function ({ api, event }) { 
     const tid = event.threadID;
-    api.sendMessage(tid, event.threadID);
-    };
-
-    
-    
+    // Corrected the message format for sendMessage
+    api.sendMessage(`Thread ID: ${tid}`, event.threadID);  // Sending the thread ID back to the thread
+};
